9aa646b7c49019401c3e0d162c40a7ec024852fc
Pax1601 <d.passed@libero.it>
2021-04-18 19:13:47 +0200

    Added mission file uploading, changed file transfer from binary to base64 string

---
+++ app.py
@@ -0,0 +1,136 @@
+from backend.mission_editing.edit_mission import MissionEditor
+from backend.data_types import Mission, WayPoint
+from backend.utils import *
+
+from typing import Union, List
+from fastapi import FastAPI
+import uvicorn
+import json
+import os
+import base64
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def is_alive():
+    return "I'm Alive!"
+
+
+@app.post("/mission")
+def new_mission(mission: Mission):
+    # create mission file to later manipulate
+    with open(f"backend\\temp_files\\missions\\{mission.session_id}.miz", 'wb') as file:
+        file.write(base64.decodebytes(mission.data))
+    # save metadata about the mission
+    data = get_dictionary()
+    with open(f"backend\\temp_files\\dictionary.json", 'w') as file:
+        data.update({mission.session_id: {"mission_name": mission.name,
+                                          "waypoints": []
+                                          }})
+        json.dump(data, file)
+
+
+@app.delete("/mission/{session_id}")
+def delete_mission(session_id: str):
+    # delete the mission file
+    os.remove(f"backend\\temp_files\\missions\\{session_id}.miz")
+    # delete mission metadata
+    data = get_dictionary()
+    with open(f"backend\\temp_files\\dictionary.json", 'w') as file:
+        if session_id in data.keys():
+            data.pop(session_id)
+            json.dump(data, file)
+        else:
+            json.dump(data, file)
+            return "Session ID doesn't exist"
+
+
+@app.post("/waypoint/{session_id}")
+def add_waypoint(waypoints: Union[List[WayPoint], WayPoint], session_id: str):
+    data = get_dictionary()
+    waypoints = [waypoints] if type(waypoints) == WayPoint else waypoints
+    with open(f"backend\\temp_files\\dictionary.json", 'w') as file:
+        if session_id in data.keys():
+            existing_waypoints = data[session_id]['waypoints']
+            for waypoint in waypoints:
+                if any([wp['wp_id']==waypoint.wp_id for wp in existing_waypoints]):
+                    json.dump(data, file)
+                    return "Cannot add an existing waypoint"
+                existing_waypoints.append(waypoint.dict())
+                data[session_id].update({'waypoints': existing_waypoints})
+            json.dump(data, file)
+        else:
+            json.dump(data, file)
+            return "Session ID doesn't exist"
+
+
+@app.delete("/waypoint/{session_id}/{waypoint_id}")
+def delete_waypoint(session_id: str, waypoint_id: str):
+    data = get_dictionary()
+    with open(f"backend\\temp_files\\dictionary.json", 'w') as file:
+        if session_id in data.keys():
+            existing_waypoints = data[session_id]['waypoints']
+            for i, wp in enumerate(existing_waypoints):
+                if wp['wp_id'] == waypoint_id:
+                    existing_waypoints.remove(wp)
+                    break
+            else:
+                json.dump(data, file)
+                return "No Such Waypoint"
+            data[session_id].update({'waypoints': existing_waypoints})
+            json.dump(data, file)
+        else:
+            json.dump(data, file)
+            return "Session ID doesn't exist"
+
+
+@app.post("/waypoint/{session_id}/{waypoint_id}")
+def update_waypoint(waypoint: WayPoint, session_id: str, waypoint_id: str):
+    data = get_dictionary()
+    with open(f"backend\\temp_files\\dictionary.json", 'w') as file:
+        if session_id in data.keys():
+            existing_waypoints = data[session_id]['waypoints']
+            for i, wp in enumerate(existing_waypoints):
+                if wp['wp_id'] == waypoint_id:
+                    existing_waypoints[i] = waypoint.dict()
+                    break
+            else:
+                json.dump(data, file)
+                return "No Such Waypoint"
+            data[session_id].update({'waypoints': existing_waypoints})
+            json.dump(data, file)
+        else:
+            json.dump(data, file)
+            return "Session ID doesn't exist"
+
+
+@app.get("/process_mission/{session_id}")
+def process_mission(session_id: str):
+    data = get_dictionary()
+    if session_id in data.keys():
+        me = MissionEditor(f"backend\\temp_files\\missions\\{session_id}.miz")
+        waypoints = []
+        for waypoint in data[session_id]['waypoints']:
+            waypoints.append(WayPoint.parse_obj(waypoint))
+        me.edit_waypoints(waypoints)
+        with open(f"backend\\temp_files\\missions\\{session_id}.miz", 'r') as file:
+            output = Mission(data=file.read(), name=data[session_id]['mission_name'], session_id=session_id)
+            return output.dict()
+    else:
+        return "Session ID doesn't exist"
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=5000, log_level="info")
--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,6 @@
 import json
 
 def get_dictionary():
-    with open(f"temp_files\\dictionary.json", 'r') as file:
+    with open(f"backend\\temp_files\\dictionary.json", 'r') as file:
         data = json.load(file)
     return data
