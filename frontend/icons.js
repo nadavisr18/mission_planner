551c6dc2c30c93031968245189792ecc22db5a3f
Pax1601 <d.passed@libero.it>
2021-04-19 20:32:40 +0200

    Minor graphic changes

--- frontend/icons.js
+++ frontend/icons.js
@@ -1,132 +1,132 @@
 
 const AnchorIcon = L.divIcon({
-    html: '<i class="fas fa-anchor" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-anchor" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'AnchorIcon'
 });
 
 const AnchorIconSelected = L.divIcon({
-    html: '<i class="fas fa-anchor" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-anchor" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'AnchorIcon'
 });
 
 const RouteIcon = L.divIcon({
-    html: '<i class="fas fa-play" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-play" style="padding-left: 4px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'RouteIcon'
 });
 
 const RouteIconSelected = L.divIcon({
-    html: '<i class="fas fa-play" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-play" style="padding-left: 4px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'RouteIcon'
 });
 
 const IPIcon = L.divIcon({
-    html: '<i class="fas fa-sign-in-alt" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-sign-in-alt" style="padding-left: 1px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'IPIcon'
 });
 
 const IPIconSelected = L.divIcon({
-    html: '<i class="fas fa-sign-in-alt" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-sign-in-alt" style="padding-left: 1px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'IPIcon'
 });
 
 const TargetIcon = L.divIcon({
-    html: '<i class="fas fa-crosshairs" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-crosshairs" style="padding-left: 2px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'TargetIcon'
 });
 
 const TargetIconSelected = L.divIcon({
-    html: '<i class="fas fa-crosshairs" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-crosshairs" style="padding-left: 2px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'TargetIcon'
 });
 
 const FACIcon = L.divIcon({
-    html: '<i class="fas fa-eye" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-eye" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'FACIcon'
 });
 
 const FACIconSelected = L.divIcon({
-    html: '<i class="fas fa-eye" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-eye" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'FACIcon'
 });
 
 const SAMIcon = L.divIcon({
-    html: '<i class="fas fa-exclamation-circle" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-exclamation-circle" style="padding-left: 1px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'SAMIcon'
 });
 
 const SAMIconSelected = L.divIcon({
-    html: '<i class="fas fa-exclamation-circle" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-exclamation-circle" style="padding-left: 1px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'SAMIcon'
 });
 
 const HomeBaseIcon = L.divIcon({
-    html: '<i class="fas fa-home" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-home" style="padding-left: 1px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'HomeBaseIcon'
 });
 
 const HomeBaseIconSelected = L.divIcon({
-    html: '<i class="fas fa-home" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-home" style="padding-left: 1px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'HomeBaseIcon'
 });
 
 const TankerIcon = L.divIcon({
-    html: '<i class="fas fa-battery-quarter" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-battery-quarter" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'TankerIcon'
 });
 
 const TankerIconSelected = L.divIcon({
-    html: '<i class="fas fa-battery-quarter" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-battery-quarter" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'TankerIcon'
 });
 
 const ContestedAreaIcon = L.divIcon({
-    html: '<i class="fab fa-fort-awesome" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fab fa-fort-awesome" style="padding-left: 2px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'ContestedAreaIcon'
 });
 
 const ContestedAreaIconSelected = L.divIcon({
-    html: '<i class="fab fa-fort-awesome" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fab fa-fort-awesome" style="padding-left: 2px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'ContestedAreaIcon'
 });
 
 const BullseyeIcon = L.divIcon({
-    html: '<i class="fas fa-bullseye" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-bullseye" style="padding-left: 2px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'BullseyeIcon'
 });
 
 const BullseyeIconSelected = L.divIcon({
-    html: '<i class="fas fa-bullseye" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-bullseye" style="padding-left: 2px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'BullseyeIcon'
 });
 
 const AirportIcon = L.divIcon({
-    html: '<i class="fas fa-fighter-jet" style="font-size: 24px; color: #212d3c;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-fighter-jet" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #212d3c;"></i><div>',
+    iconSize: [30, 30],
     className: 'AirportIcon'
 });
 
 const AirportIconSelected = L.divIcon({
-    html: '<i class="fas fa-fighter-jet" style="font-size: 24px; color: #2196F3;"></i>',
-    iconSize: [24, 24],
+    html: '<div class="icon-background"><i class="fas fa-fighter-jet" style="padding-left: 0px; padding-top: 2px; font-size: 20px; color: #2196F3;"></i><div>',
+    iconSize: [30, 30],
     className: 'AirportIcon'
 });
