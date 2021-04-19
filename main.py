b612ae4d4dd335296bc4d08a7282aff1e883dbe6
Nadav <nadavf8@gmail.com>
2021-04-19 22:44:40 +0300

    added some todos

--- main.py
+++ main.py
@@ -5,12 +5,15 @@
 # TODO: save waypoint plans
 # TODO: clean temp files every X time
 # TODO: add layers the user can load
+# TODO: add group name to waypoint planner
 
 # -------------------FRONTEND-------------------
 # TODO: add radio tab that has a drop down of planes and looks like [channel] : [frequency]
 # TODO: When the user picks a waypoint type that's one of: target, SAM, airport, home base, contested area, have the altitude be 0 and AGL, and the fields will be greyed out
 # TODO: add toolbar with map type selection, undo and redo buttons, clear map button and more if you can think of any
 # TODO: add white background for waypoint symbols
+# TODO: have aircraft dropdown taken from the aircraft types that exist in the uploaded mission
+# TODO: grey out all fields until a mission has been uploaded
 
 
 # -------------------QUESTIONS-------------------
