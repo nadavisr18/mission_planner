a3d803ab83e4beb48241e0a33c8d191cfdc7a357
Nadav <nadavf8@gmail.com>
2021-04-27 20:44:12 +0300

    added todos

--- main.py
+++ main.py
@@ -5,11 +5,20 @@
 # TODO: save waypoint plans
 # TODO: clean temp files every X time
 # TODO: add layers the user can load
+# TODO: find aircraft spawn places and display them on mission upload
+# TODO: add color mappings for each aircraft for DCS
 
 # -------------------FRONTEND-------------------
 # TODO: fix size of icons
 # TODO: what are the acceptable image formats for the kneeboard?
 # TODO: add discord link
 # TODO: add list of uploaded kneeboard files
-
+# TODO: have discord link open in new tab
+# TODO: have upload new mission button and an apply changes and download button
+# TODO: have everyone option in radios
+# TODO: popup that asks if you want to overwrite existing radios
+# TODO: add placeholder for radio lables
+# TODO: increase outline in waypoint text
+# TODO: add color mappings for each aircraft for DCS
+# TODO: weather!
 
