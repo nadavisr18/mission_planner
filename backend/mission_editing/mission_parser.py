372ec97abd386450caf65ba54c7742829f81d873
Nadav <nadavf8@gmail.com>
2021-10-03 19:28:19 +0300

    fixed group type issue

--- backend/mission_editing/mission_parser.py
+++ backend/mission_editing/mission_parser.py
@@ -16,19 +16,23 @@ class MissionParser(MissionEditor):
                 country_dict = self.mission['coalition']['blue']['country'][country]
                 for group_type in group_types:
                     if group_type in country_dict.keys():
-                        for group in country_dict[group_type]['group']:
-                            group_dict = country_dict['plane']['group'][group]
-                            unit_type = group_dict['units'][1]['type']
-                            x, y = group_dict['x']/111139, group_dict['y']/111139
-                            lat_diff, lon_diff = self.xy2ll_model.predict([[x, y], ])[0]
-                            lat, lon = self.map_center['lat'] + lat_diff, self.map_center['lon'] + lon_diff
-                            group_data = Group(group_type=group_type,
-                                               unit_type=unit_type,
-                                               name=group_dict['name'],
-                                               country=country,
-                                               coalition=coalition,
-                                               lat=lat,
-                                               lon=lon)
-                            groups.append(group_data)
+                        if group_type == 'static':
+                            groups_data = self.create_static_groups(country_dict[group_type]['group'])
+                            groups.extend(groups_data)
+                        else:
+                            for group in country_dict[group_type]['group']:
+                                group_dict = country_dict[group_type]['group'][group]
+                                unit_type = group_dict['units'][1]['type']
+                                x, y = group_dict['x'] / 111139, group_dict['y'] / 111139
+                                lat_diff, lon_diff = self.xy2ll_model.predict([[x, y], ])[0]
+                                lat, lon = self.map_center['lat'] + lat_diff, self.map_center['lon'] + lon_diff
+                                group_data = Group(group_type=group_type,
+                                                   unit_type=unit_type,
+                                                   name=group_dict['name'],
+                                                   country=country,
+                                                   coalition=coalition,
+                                                   lat=lat,
+                                                   lon=lon)
+                                groups.append(group_data)
 
         return groups, self.mission['theatre']
