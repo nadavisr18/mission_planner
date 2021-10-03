ce5147ff27963335326e2eae5f2479353b3e327c
Nadav <nadavf8@gmail.com>
2021-10-03 12:52:53 +0300

    added some TODOs

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 # TODO: add customizable weather options
 # TODO: save waypoint plans
 # TODO: clean temp files every X time
-# TODO: add layers the user can load
 # TODO: find aircraft spawn places and display them on mission upload
-# TODO: add color mappings for each aircraft for DCS
-# TODO: return link to weather icon image
 
 # -------------------FRONTEND-------------------
-# TODO: fix size of icons
 # TODO: add color mappings for each aircraft for DCS
-# TODO: weather!
 # TODO: fix when selecting a new aircraft in the radio tab it doesn't reset the first radio options
+# TODO: add more map source options
+# TODO: add random weather button
+# TODO: add weather status display with icon and wind
+# TODO: show groups on the map on mission file load
