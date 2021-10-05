a27941f313e50cb800f561aeeb0eaf7338b9f516
Nadav <nadavf8@gmail.com>
2021-10-05 18:56:22 +0300

    TODOs

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 # -------------------BACKEND-------------------
 # TODO: save waypoint plans
 # TODO: clean temp files every X time
-# TODO: find aircraft spawn places and display them on mission upload
-# TODO: join together close static object into one group
-# TODO: recognize static groups > 5 pieces under x meters, if it has a FARP, else generic enemy base
 
 # -------------------FRONTEND-------------------
 # TODO: add color mappings for each aircraft for DCS
 # TODO: fix when selecting a new aircraft in the radio tab it doesn't reset the first radio options
 # TODO: add more map source options
 # TODO: add random weather button
-# TODO: add weather status display with icon and wind
-# TODO: show groups on the map on mission file load
+# TODO: match up the icons file names with the DCS plane names
+# TODO: get icons for "home base" and "contested area" type waypoints
+# TODO: only be able to edit waypoints for client groups
+# TODO: be able to delete the selected waypoint by pressing the "DELETE" key on the keyboard
