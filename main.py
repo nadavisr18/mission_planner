47f946338fe0e063e1c916389978a1f8851000d7
Nadav <nadavf8@gmail.com>
2021-10-25 15:33:10 +0300

    TODOs

--- main.py
+++ main.py
@@ -6,7 +6,11 @@
 # TODO: return AI flights waypoints on mission load
 
 # -------------------FRONTEND-------------------
+# TODO: fix static groups not showing at all
+# TODO: add to the top toolbar option to hide country flags
+# TODO: fix waypoint line going through the top of the waypoint icon and not center
 # TODO: get icons for "home base" and "contested area" type waypoints
+# TODO: add tooltips for the top tool bar
 # TODO: display AI routes on map (waiting for backend to finish first), have different route color to Client flights
 # TODO: display current weather on mission load (waiting for backend to finish first)
 # TODO: add random weather button (just send the word "random" instead of a city name)
