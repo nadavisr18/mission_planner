693c7dab57271e5e3c2742da134b2c124cc3211b
dpassoni <davide.passoni@positech.it>
2021-10-25 10:28:46 +0200

    Updated TODOs

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 # TODO: return AI flights waypoints on mission load
 
 # -------------------FRONTEND-------------------
-# TODO: only be able to edit waypoints for client groups
 # TODO: get icons for "home base" and "contested area" type waypoints
 # TODO: display AI routes on map (waiting for backend to finish first), have different route color to Client flights
 # TODO: display current weather on mission load (waiting for backend to finish first)
