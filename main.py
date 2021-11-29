7d1c04e09502455ffec55dab1dae63473b90dff3
dpassoni <davide.passoni@positech.it>
2021-11-29 17:34:57 +0100

    better handling of waypoint selection and editing.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@
 # -------------------V2-------------------
 # TODO: add more map source options
 # TODO: come up with a better icon for non-client aircraft
-# TODO: add some way to selectively hide flights
 # TODO: add some way to hide waypoints only
 # TODO: when a user selects a waypoint, open the waypoint tab. When a user de-selects a waypoint close the tab and don't add more waypoints when they press on the map
 # TODO: create a tab for package management
 # TODO: find a way to add a waypoint in between existing waypoints
+# TODO: differentiate between client and non client waypoints
 
