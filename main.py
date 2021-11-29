763cebce6d4392781dcc0ecc06f76e8ce1ea6097
Nadav <nadavf8@gmail.com>
2021-11-29 21:14:49 +0200

    TODOs

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 
 # -------------------BACKEND-------------------
-# TODO: fix in game time bug
+# -------------------V1-------------------
+# -------------------V2-------------------
 # TODO: auto kneeboards
+# TODO: add package detection
 
 # -------------------FRONTEND-------------------
 # -------------------V1-------------------
-# ALL DONE: YAY!
 # -------------------V2-------------------
+# TODO: when radio tab is open and user clicks on a group have it selected (like you do with waypoints)
+# TODO: make the EVERYONE option fist in all the relevant drop-downs
 # TODO: add more map source options
 # TODO: come up with a better icon for non-client aircraft
 # TODO: add some way to hide waypoints only
-# TODO: when a user selects a waypoint, open the waypoint tab. When a user de-selects a waypoint close the tab and don't add more waypoints when they press on the map
 # TODO: create a tab for package management
 # TODO: find a way to add a waypoint in between existing waypoints
 # TODO: differentiate between client and non client waypoints
