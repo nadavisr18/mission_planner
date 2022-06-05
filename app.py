e55b74dc222ec7b0fe830fbcef10c5879c979807
Nadav's Laptop <nadavf8@gmail.com>
2022-06-05 22:22:35 +0300

    hosting address

--- app.py
+++ app.py
@@ -232,4 +232,4 @@ def get_progress() -> float:
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, log_level="info")
