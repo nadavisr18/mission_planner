dca940a60c36bb29ec12b2865fa29cbc83054b55
nadavf8@gmail.com <nadavf8@gmail.com>
2022-06-08 22:57:27 +0300

    changed url to server

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
-var serverAddress = "http://127.0.0.1:5000";
+var serverAddress = "https://mission-planner-r74p3ivczq-ew.a.run.app";
 
 /* Dropdown selection values */
 var selections = {"waypoint-type": ["anchor", "route", "ip", "target", "fac", "sam", "home base", "tanker", "contested area", "bullseye", "airport"],
