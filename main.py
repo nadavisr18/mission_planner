5d27a676d6520a0eb8ce7cce01269223a4ff5882
Nadav's Laptop <nadavf8@gmail.com>
2022-06-09 20:34:23 +0300

    edited todos

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 # -------------------BACKEND-------------------
 # -------------------V1-------------------
+# TODO: fix lua comments breaking mission load
 # -------------------V2-------------------
 # TODO: auto kneeboards
 # TODO: add package detection
 
 # -------------------FRONTEND-------------------
 # -------------------V1-------------------
+# TODO: add icons for: P-47, MiG-27K
+# TODO: slow down progress requests
 # -------------------V2-------------------
-# TODO: add more map source options
 # TODO: create a tab for package management
