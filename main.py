1825165bf1d713eb1d3fbf7963345df2dd6bc2b3
Nadav's Laptop <nadavf8@gmail.com>
2022-06-10 21:06:19 +0300

    TODOs

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 # TODO: center the view on the average client group coordinates
 #  (now when loading a mission to Caucasus the view still is on Syria and you need to pan it over yourself)
 # TODO: when pressing on a client group with waypoints, select the last one for edit
+# TODO: EVERYONE waypoints don't work
 # TODO: when pressing on a non-client group with the waypoint tab open, place the waypoint on top of the group
 # TODO: add icons for: P-47, MiG-27K
 # TODO: when selecting a waypoint the altimeter toggle switch position will change
