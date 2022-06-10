8312bec2fe55c0941478b95cb420b23def6582d4
Nadav's Laptop <nadavf8@gmail.com>
2022-06-10 15:41:37 +0300

    TODOs

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 
 # -------------------BACKEND-------------------
 # -------------------V1-------------------
-# TODO: fix lua comments breaking mission load
 # -------------------V2-------------------
 # TODO: auto kneeboards
 # TODO: add package detection
 
 # -------------------FRONTEND-------------------
 # -------------------V1-------------------
-# TODO: add icons for: P-47, MiG-27K
-# TODO: hide randomly generated (hash) static unit names
+# TODO: fix visibility cookies not an array bug
 # TODO: center the view on the average client group coordinates
-#  (now when loading a mission to Caucasus the view still is on Syria)
+#  (now when loading a mission to Caucasus the view still is on Syria and you need to pan it over yourself)
+# TODO: when pressing on a client group with waypoints, select the last one for edit
+# TODO: when pressing on a non-client group with the waypoint tab open, place the waypoint on top of the group
+# TODO: add icons for: P-47, MiG-27K
+# TODO: when selecting a waypoint the altimeter toggle switch position will change
+#  but the text inside (AGL/ASL) does not change to the selected waypoint's setting
 # -------------------V2-------------------
 # TODO: create a tab for package management
